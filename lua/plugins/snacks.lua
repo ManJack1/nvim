22821f8a3a8c56f35c570593f53b259e281e8dbf
manjack <manjack0910@gmail.com>
2025-07-11 16:56:45 +0800

    feat(snacks): enable inline image preview
    Updated the configuration for the 'snacks' plugin to enable the inline
    image viewer. This change allows images to be rendered directly within
    the buffer for supported file types like Markdown and HTML.
    
    Specifically, the  and  options were set to .
    The floating window option () was disabled as the inline view
    is now prioritized.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,14 +11,14 @@ return {
         -- enable image viewer for documents
         -- a treesitter parser must be available for the enabled languages.
         -- supported language injections: markdown, html
-        enabled = false,
+        enabled = true,
         -- render the image inline in the buffer
         -- if your env doesn't support unicode placeholders, this will be disabled
         -- takes precedence over `opts.float` on supported terminals
-        inline = false,
+        inline = true,
         -- render the image in a floating window
         -- only used if `opts.inline` is disabled
-        float = true,
+        -- float = true,
         max_width = 40,
         max_height = 40,
         math = {
