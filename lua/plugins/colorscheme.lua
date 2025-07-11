686e26f8878a050f881905731506c755497b56a4
manjack <manjack0910@gmail.com>
2025-07-11 18:00:53 +0800

    feat(colorscheme): switch to dracula theme
    Replaced the "gruvbox" colorscheme with "dracula" by updating the plugin
    source and the colorscheme command. This ensures the new theme loads
    with
    high priority and sets the background to dark mode.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "Mofiqul/dracula.nvim",
   priority = 1000, -- Ensure it loads first
   config = function()
     vim.o.background = "dark" -- or "light" for light mode
-    vim.cmd([[colorscheme gruvbox]])
+    vim.cmd([[colorscheme dracula]])
   end,
 }
