fae8a4e6bfd04b1f4cedaede2b0afe958030f31a
Your Name <you@example.com>
2025-09-21 15:53:46 +0800

    feat(conform): enhance injected formatter options
    Refactor conform.nvim config to add detailed options for injected
    formatters. Introduce explicit mappings for language to filetype and
    extension, and set ignore_errors to false for injected formatting.
    This improves flexibility and accuracy when formatting code blocks
    injected via treesitter.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,13 +2,34 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters = {
-      options = {
-        ignore_errors = true,
-        lang_to_formatters = {
-          cpp = "clang-format",
-          python = { "isort", "black" },
-          c = "clang-format",
-          lua = "stylua",
+      injected = {
+        options = {
+          -- Set to true to ignore errors
+          ignore_errors = false,
+          -- Map of treesitter language to filetype
+          lang_to_ft = {
+            bash = "sh",
+          },
+          -- Map of treesitter language to file extension
+          -- A temporary file name with this extension will be generated during formatting
+          -- because some formatters care about the filename.
+          lang_to_ext = {
+            bash = "sh",
+            c_sharp = "cs",
+            elixir = "exs",
+            javascript = "js",
+            julia = "jl",
+            latex = "tex",
+            markdown = "md",
+            python = "py",
+            ruby = "rb",
+            rust = "rs",
+            teal = "tl",
+            typescript = "ts",
+          },
+          -- Map of treesitter language to formatters to use
+          -- (defaults to the value from formatters_by_ft)
+          lang_to_formatters = {},
         },
       },
     },
