ece800719428ae79b638e8e8944e94d2e00617fa
manjack <manjack0910@gmail.com>
2025-10-01 03:46:46 +0800

    feat(plugins): cleanup snippets and diffview configs
    Remove unused snippet and diffview plugin configs for a leaner setup.
    Update compiler.nvim dependencies to include telescope.nvim. This
    streamlines
    plugin management and removes redundant or disabled configurations.

--- lua/plugins/compile-nvim.lua
+++ lua/plugins/compile-nvim.lua
@@ -1,65 +1,9 @@
 return {
-  -- { -- This plugin
-  --   "Zeioth/compiler.nvim",
-  --   cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-  --   dependencies = { "stevearc/overseer.nvim" },
-  --   opts = {},
-  --   keys = {
-  --     { "mm", "<cmd>CompilerOpen<cr>", desc = "CompilerOpen" },
-  --     { "mt", "<cmd>CompilerToggleResults<cr>", desc = "CompilerToggleResults" },
-  --   },
-  -- },
-  -- -- { -- The task runner we use
-  -- --   "stevearc/overseer.nvim",
-  -- --   commit = "3047ede61cc1308069ad1184c0d447ebee92d749",
-  -- --   cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-  -- --   opts = {
-  -- --     task_list = {
-  -- --       direction = "bottom",
-  -- --       min_height = 25,
-  -- --       max_height = 25,
-  -- --       default_detail = 1,
-  -- --       bindings = {
-  -- --         ["q"] = function()
-  -- --           vim.cmd("OverseerClose")
-  -- --         end,
-  -- --       },
-  -- --     },
-  -- --   },
-  -- -- },
-  -- { -- The task runner we use
-  --   "stevearc/overseer.nvim",
-  --   commit = "19aac0426710c8fc0510e54b7a6466a03a1a7377",
-  --   cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-  --   opts = {
-  --     task_list = {
-  --       direction = "bottom",
-  --       min_height = 25,
-  --       max_height = 25,
-  --       default_detail = 1,
-  --       bindings = {
-  --         ["q"] = function()
-  --           vim.cmd("OverseerClose")
-  --         end,
-  --       },
-  --     },
-  --   },
-  -- },
   { -- This plugin
     "Zeioth/compiler.nvim",
     cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
     dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
     opts = {},
-    -- keys = {
-    --   { "mm", "<cmd>CompilerOpen<cr>", desc = "CompilerOpen" },
-    --   { "mt", "<cmd>CompilerToggleResults<cr>", desc = "CompilerToggleResults" },
-    -- },
-    -- config = function()
-    --   require("which-key").add({
-    --     { "mm", "<cmd>CompilerOpen<cr>", desc = "CompilerOpen" },
-    --     { "mt", "<cmd>CompilerToggleResults<cr>", desc = "CompilerToggleResults" },
-    --   })
-    -- end,
   },
   { -- The task runner we use
     "stevearc/overseer.nvim",
