6ead31bed51372347ee2f3ea582447a043723029
Manjack <manjack0910@gmail.com>
2025-10-08 07:47:19 +0800

    feat(snacks): move image config under opts
    Refactored the snacks.nvim plugin configuration by relocating the image
    settings from the top-level to the `opts` table. This change improves
    configuration structure and consistency. Updated the `float` option to
    `false` for inline image rendering in documents.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,35 +3,34 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-
+    image = {
+      ---@class snacks.image.Config
+      enabled = true,
+      doc = {
+        -- enable image viewer for documents
+        -- a treesitter parser must be available for the enabled languages.
+        -- supported language injections: markdown, html
+        enabled = true,
+        -- render the image inline in the buffer
+        -- if your env doesn't support unicode placeholders, this will be disabled
+        -- takes precedence over `opts.float` on supported terminals
+        inline = true,
+        -- render the image in a floating window
+        -- only used if `opts.inline` is disabled
+        float = false,
+        max_width = 40,
+        max_height = 40,
+        math = {
+          latex = {
+            font_size = "normalsize",
+          },
+        },
+      },
+    },
     picker = {
       sources = {
         noice = {
           confirm = { "yank", "close" },
-        },
-      },
-    },
-  },
-  image = {
-    ---@class snacks.image.Config
-    enabled = true,
-    doc = {
-      -- enable image viewer for documents
-      -- a treesitter parser must be available for the enabled languages.
-      -- supported language injections: markdown, html
-      enabled = true,
-      -- render the image inline in the buffer
-      -- if your env doesn't support unicode placeholders, this will be disabled
-      -- takes precedence over `opts.float` on supported terminals
-      inline = true,
-      -- render the image in a floating window
-      -- only used if `opts.inline` is disabled
-      float = true,
-      max_width = 40,
-      max_height = 40,
-      math = {
-        latex = {
-          font_size = "normalsize",
         },
       },
     },
